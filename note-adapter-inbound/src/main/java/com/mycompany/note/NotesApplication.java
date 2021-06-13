b9a6c25bb9a2ad796e90178cb22637eaf6a3aeeb
DiannaAsatryan <diana.asatryan@gmail.com>
2021-06-13 18:36:09 +0400

    success build

--- note-adapter-inbound/src/main/java/com/mycompany/note/NotesApplication.java
+++ note-adapter-inbound/src/main/java/com/mycompany/note/NotesApplication.java
@@ -1,4 +1,4 @@
-package com.mycompany.notes;
+package com.mycompany.note;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- note-domain/src/main/java/com/mycompany/note/domain/service/NoteService.java
+++ note-domain/src/main/java/com/mycompany/note/domain/service/NoteService.java
@@ -1,9 +1,10 @@
 package com.mycompany.note.domain.service;
 
 import com.mycompany.note.domain.model.Note;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+@Service
 public interface NoteService {
 
     List<Note> getAllNotes(long userId);
